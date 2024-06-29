d46c1ca0e640dad0021ba44ce72c4c44ed2ad4c6
조영웅 <120241376+JoJimi@users.noreply.github.com>
2024-06-30 01:56:40 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1664 ms, Memory: 384144 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+    static int[][] graph;
+    static int[][][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        graph = new int[N+1][M+1];
+        visited = new int[K+1][N+1][M+1];
+
+        for(int i = 1; i <= N; i++){
+            String str = br.readLine();
+            for(int j = 1; j <= M; j++){
+                graph[i][j] = str.charAt(j-1) - '0';
+            }
+        }
+        if(N==1 && M==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(bfs());
+    }
+    static public int bfs(){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 1, 1});
+        visited[0][1][1] = 1;
+
+        while(!q.isEmpty()){
+            int[] pol = q.poll();
+
+            for(int i = 0; i < 4; i++){
+                int nx = pol[1] + dx[i];
+                int ny = pol[2] + dy[i];
+
+                if(nx < 1 || nx > N || ny < 1 || ny > M) continue;
+
+                if(graph[nx][ny] == 0){
+                    if(visited[pol[0]][nx][ny] == 0){
+                        q.add(new int[]{pol[0], nx, ny});
+                        visited[pol[0]][nx][ny] = visited[pol[0]][pol[1]][pol[2]] + 1;
+
+                        if(nx == N && ny == M) return visited[pol[0]][nx][ny];
+                    }
+                }
+                else{
+                    if(pol[0] < K){
+                        if(visited[pol[0] + 1][nx][ny] == 0){
+                            q.add(new int[]{pol[0] + 1, nx, ny});
+                            visited[pol[0] + 1][nx][ny] = visited[pol[0]][pol[1]][pol[2]] + 1;
+
+                            if(nx == N && ny == M) return visited[pol[0] + 1][nx][ny];
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
