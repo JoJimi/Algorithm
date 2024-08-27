a0cab984aa1b3d2e502e34fe512a0644dbd91275
조영웅 <120241376+JoJimi@users.noreply.github.com>
2024-08-27 23:16:31 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    static long N, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Long.parseLong(br.readLine());
+        res = N;
+
+        for(long i = 2; i <= Math.sqrt(N); i++){
+            if(N % i == 0){
+                res -= (res / i);
+
+                while(N % i == 0){
+                    N /= i;
+                }
+            }
+        }
+        if(N > 1) {
+            res -= (res / N);
+        }
+
+        System.out.println(res);
+    }
+}
