989b6074492777103d1892fdbabcdc40a0d08cb6
조영웅 <120241376+JoJimi@users.noreply.github.com>
2024-10-29 18:27:16 +0900

    [Gold II] Title: 줄 세우기, Time: 444 ms, Memory: 91172 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.java
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, max = 0;
+    static int N, max = 1;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
