b3c43910f0e57aba78ff7ee2402116e295a69d86
조영웅 <120241376+JoJimi@users.noreply.github.com>
2025-02-28 23:46:44 +0900

    [Gold III] Title: 사다리 조작, Time: 4784 ms, Memory: 16616 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, H;
+    static int[][] ladder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        if(M >= 1) ladder = new int[H][N];
+        else {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            ladder[a-1][b-1] = 1;
+        }
+
+        for(int i = 0; i < 4; i++){
+            solution(0, 0, i);
+        }
+
+        System.out.println(-1);
+    }
+    public static boolean check(){
+        for(int i = 0; i < N; i++){
+            int line = i;
+            int height = 0;
+
+            while(height < H){
+                if(ladder[height][line]==1){
+                    line++;
+                }
+                else if(line-1 >= 0 && ladder[height][line-1]==1){
+                    line--;
+                }
+                height++;
+            }
+
+            if(line != i) return false;
+        }
+
+        return true;
+    }
+
+    public static void solution(int x, int count, int size){
+        if(count > 3) return;
+        if(count == size){
+            if(check()){
+                System.out.println(count);
+                System.exit(0);
+            }
+        }
+
+        for(int i = x; i < H; i++){
+            for(int j = 0; j < N-1; j++){
+                if(ladder[i][j] == 0){
+                    ladder[i][j] = 1;
+                    solution(i, count+1, size);
+                    ladder[i][j] = 0;
+                }
+            }
+        }
+    }
+}
