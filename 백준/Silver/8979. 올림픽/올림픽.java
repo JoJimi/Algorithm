b470340795bc399b396daa5bec70a6fe48cdac0b
조영웅 <120241376+JoJimi@users.noreply.github.com>
2025-04-06 00:44:31 +0900

    [Silver V] Title: 올림픽, Time: 104 ms, Memory: 14208 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[][] country;
+    static int[] rank;
+    static List<int[]> sortedList = new ArrayList<>();
+    static PriorityQueue<int[]> pq = new PriorityQueue<>( (o1, o2) -> {
+        if (o1[1] != o2[1]) {
+            return o2[1] - o1[1];
+        }
+        if (o1[2] != o2[2]) {
+            return o2[2] - o1[2];
+        }
+        if (o1[3] != o2[3]) {
+            return o2[3] - o1[3];
+        }
+        return o1[0] - o2[0];
+    });
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        country = new int[N][4];
+        rank = new int[N+1];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 4; j++){
+                country[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int[] arr : country) {
+            pq.offer(arr);
+        }
+
+        while (!pq.isEmpty()) {
+            sortedList.add(pq.poll());
+        }
+
+        int count = 0;
+        int prevGold = -1, prevSilver = -1, prevBronze = -1;
+
+        for(int i = 0; i < N; i++){
+            int[] result = sortedList.get(i);
+
+            if(prevGold == result[1] && prevSilver == result[2] && prevBronze == result[3]){
+                rank[result[0]] = count;
+            }
+            else {
+                count++;
+                rank[result[0]] = count;
+            }
+
+            prevGold = result[1];
+            prevSilver = result[2];
+            prevBronze = result[3];
+        }
+
+        System.out.println(rank[K]);
+    }
+}
