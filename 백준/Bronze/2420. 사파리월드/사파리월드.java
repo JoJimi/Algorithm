3bde1ecd5d316ad1dd7aa5d6dab2524b0d26f270
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-02-06 16:49:20 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long N, M;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.abs(N-M));
+
+    }
+}
