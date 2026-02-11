30b02efee6e7016109b4c106aa0d83c86e56c7a0
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-02-11 14:17:46 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
