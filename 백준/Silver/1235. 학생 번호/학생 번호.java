4cfaf87a200b7e2096f2435bebe91251ff2c5e19
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-02-14 17:15:45 +0900

    [Silver IV] Title: 학생 번호, Time: 124 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.java
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -4,30 +4,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int k = 0;
 
+        int N = Integer.parseInt(br.readLine());
         String[] str = new String[N];
+
         for(int i = 0; i < N; i++){
             str[i] = br.readLine();
         }
-        for(int len = 1; len < str[0].length(); len++){
-            boolean judge = false;
-            for(int i = 0; i < N-1; i++){
-                String str1 = str[i].substring(str[i].length() - len);
-                for(int j = i+1; j < N; j++){
-                    String str2 = str[j].substring(str[j].length() - len);
 
-                    if (str1.equals(str2)) {
-                        judge = true;
-                        break;
-                    }
+        int L = str[0].length();
+
+        for(int len = 1; len <= L; len++){
+            boolean judge = false;
+            HashSet<String> set = new HashSet<>();
+
+            for(int i = 0; i < N; i++){
+                String suffix = str[i].substring(L - len);
+
+                if(!set.add(suffix)){
+                    judge = true;
+                    break;
                 }
             }
-            if(judge) k++;
-            else break;
-        }
 
-        System.out.println(k+1);
+            if(!judge) {
+                System.out.println(len);
+                break;
+            }
+        }
     }
 }
