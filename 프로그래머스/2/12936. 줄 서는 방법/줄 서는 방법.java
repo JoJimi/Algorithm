330ad7328824ba1c81bddfc258d28d0dc605dd7b
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-03-17 17:12:48 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -5,26 +5,25 @@ class Solution {
         int[] answer = new int[n];
         
         List<Integer> nums = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++) {
             nums.add(i);
         }
         
         long[] fact = new long[n+1];
         fact[0] = 1;
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1 ; i <= n; i++){
             fact[i] = fact[i-1] * i;
         }
         
         k--;
         
         for(int i = 0; i < n; i++) {
-            long f = fact[n - 1 - i];
-            int index = (int)(k / f);
+            int index = (int)(k / fact[n - 1 - i]);
             
             answer[i] = nums.get(index);
             nums.remove(index);
             
-            k %= f;
+            k %= fact[n - 1 - i];
         }
         
         return answer;
