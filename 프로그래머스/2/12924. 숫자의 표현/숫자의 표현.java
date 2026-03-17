665cdd3270af28137b2528954f7d945b186fb670
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-03-17 12:03:27 +0900

    [level 2] Title: 숫자의 표현, Time: 0.11 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,27 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int sum = 0;
-        List<Integer> sums = new ArrayList<>();
-        for(int i = 1; i < 300; i++) {
-            sum += i;
-            sums.add(sum);
-        }
-        
-        for(int i = 0; i < 299; i++) {
-            int num = sums.get(i);
-            int diff = n - num;
-            
-            if(n < num) {
-                break;
-            }
-            
-            if(diff % (i+1) == 0) {
-                answer++;
-            }
+        for(int i = 1; i <= n; i+=2) {
+            if(n % i == 0) answer++;
         }
         
         return answer;
