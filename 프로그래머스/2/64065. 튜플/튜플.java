2bd2a51172f77b24642274e05a0c5045647aff3c
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-03-30 19:07:51 +0900

    [level 2] Title: 튜플, Time: 54.22 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,43 +1,21 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.substring(1, s.length() - 1);
+        HashSet<String> set = new HashSet<>();
+        String[] str = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(str, (o1, o2) -> {
+            return o1.length() - o2.length();
+        });
         
-        List<String> lists = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            
-            sb.append(ch);
-            if(ch == '}') {
-                lists.add(sb.toString());
-                sb = new StringBuilder();
-                i++;
-            }        
-        }       
-        Map<Integer, Integer> map = new HashMap<>();
-        while(true) {
-            if(lists.size() == 0) break;
-            String str = lists.remove(0);
-            String[] sArr = str.substring(1, str.length() - 1).split(",");
-            
-            for(int i = 0; i < sArr.length; i++) {
-                int n = Integer.parseInt(sArr[i]);
-                map.put(n, map.getOrDefault(n, 0) + 1);
-                
+        int[] answer = new int[str.length];
+        int idx = 0;
+        for(String s1 : str) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) {
+                    answer[idx++] = Integer.parseInt(s2);
+                }
             }
-        }
-        
-        List<Integer> keys = map.entrySet().stream()
-            .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-            .map(Map.Entry::getKey)
-            .collect(Collectors.toList());
-
-        int[] answer = new int[keys.size()];
-        for(int i = 0; i < keys.size(); i++) {
-            answer[i] = keys.get(i);
         }
         
         return answer;
