fabdea6c4319a24f3ef585b4d296e1aeb5dd9363
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-04-13 12:32:24 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] solution(int n) {
         int index = 0, size = n/2;
-        if(n % 2 == 1) size ++;
-        
+        if(n % 2 == 1) {
+            size++;
+        }
         int[] answer = new int[size];
+        
         for(int i = 1; i <= n; i++) {
             if(i % 2 == 1){
                 answer[index] = i;
