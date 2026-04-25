7b647d9e4e9f59326e5dd9a5399f8f3efa15b0cb
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-04-25 17:03:15 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.71 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String string) {
+        String s = string.toLowerCase();
+        
+        PriorityQueue<Character> pq = new PriorityQueue<>();
+        for(int i = 0; i < s.length(); i++) {
+            pq.add(s.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            sb.append(pq.poll());
+        }
+        
+        return sb.toString();
+    }
+}
