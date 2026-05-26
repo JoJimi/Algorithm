cf9951add3a75ee7bd1eabb297d8f199179c1f9e
JoJimi <duddnddnfl@tukorea.ac.kr>
2026-05-26 09:02:44 +0900

    [level 0] Title: OX퀴즈, Time: 0.18 ms, Memory: 61.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+
+        for (int i = 0; i < quiz.length; i++) {
+            String[] arr = quiz[i].split(" ");
+
+            int x = Integer.parseInt(arr[0]);
+            String op = arr[1];
+            int y = Integer.parseInt(arr[2]);
+            int z = Integer.parseInt(arr[4]);
+
+            int result = 0;
+
+            if (op.equals("+")) {
+                result = x + y;
+            } else {
+                result = x - y;
+            }
+
+            answer[i] = (result == z) ? "O" : "X";
+        }
+
+        return answer;
+    }
+}
